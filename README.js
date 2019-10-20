21e871d316ec7ec80c3b2ff8e994f06ec7c6c4bf
albathn <56801881+albathn@users.noreply.github.com>
2019-10-20 12:53:58 -0700

    Create README.js

---
+++ README.js
@@ -0,0 +1,12 @@
+# popup
+popunder
+<script async='async' src='https://raw.githubusercontent.com/taenks/dpopunder/master/pop1' type='text/javascript'/></script>
+<br/>
+<!-- Histats.com  START  (standard)-->
+<script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+<a href="http://www.histats.com" target="_blank" title="" ><script  type="text/javascript" >
+try {Histats.start(1,2619247,4,16,150,30,"00011100");
+Histats.track_hits();} catch(err){};
+</script></a>
+<noscript><a href="http://www.histats.com" target="_blank"><img src="http://sstatic1.histats.com/0.gif?2619247&amp;101" alt="" border="0" /></a></noscript>
+<!-- Histats.com  END  -->
