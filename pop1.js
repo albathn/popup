f67d42f294b291fb81e3580703a44bec50439bc9
albathn <56801881+albathn@users.noreply.github.com>
2020-06-22 02:30:21 +0800

    Update pop1.js

--- pop1.js
+++ pop1.js
@@ -1,2 +1,109 @@
-function jsPopunder(e,t){function d(){try{c=Math.floor(document.cookie.split(h+"Cap=")[1].split(";")[0])}catch(e){}return l<=c||document.cookie.indexOf(h+"=")!==-1}function v(e,t,i,s,o,u){if(d())return;var a="toolbar=no,scrollbars=yes,location=yes,statusbar=yes,menubar=no,resizable=1,width="+i.toString()+",height="+s.toString()+",screenX="+o+",screenY="+u,l=function(){if(d())return;r=n.window.open(e,t,a);if(r){var i=new Date,s=new Date(i.setTime(i.getTime()+f));document.cookie=h+"=1;expires="+s.toGMTString()+";path=/";var o=new Date;o.setHours(24,0,0,0),document.cookie=h+"Cap="+(c+1)+";expires="+o.toGMTString()+";path=/",m()}};document.addEventListener?document.addEventListener("click",l,!1):document.attachEvent("onclick",l)}function m(){try{r.blur(),r.opener.window.focus(),window.self.window.focus(),window.focus(),p.firefox&&g(),p.webkit&&y(),p.msie&&setTimeout(function(){r.blur(),r.opener.window.focus(),window.self.window.focus(),window.focus()},1e3)}catch(e){}}function g(){var e=window.open("about:blank");e.focus(),e.close()}function y(){var e="",t=document.createElement("a");t.href="data:text/html,window.close();",document.getElementsByTagName("body")[0].appendChild(t);var n=document.createEvent("MouseEvents");n.initMouseEvent("click",!1,!0,window,0,0,0,0,0,!0,!1,!1,!0,0,null),t.dispatchEvent(n),t.parentNode.removeChild(t)}var n=top!=self&&typeof top.document.location.toString()=="string"?top:self,r=null;t=t||{};var i=t.name||Math.floor(Math.random()*1e3+1),s=t.width||window.outerWidth||window.innerWidth,o=t.height||window.outerHeight-100||window.innerHeight,u=typeof t.left!="undefined"?t.left.toString():window.screenX,a=typeof t.top!="undefined"?t.top.toString():window.screenY,f=t.wait||3600;f*=1e3;var l=t.cap||2,c=0,h=t.cookie||"__.popunder",p=function(){var e=navigator.userAgent.toLowerCase(),t={webkit:/webkit/.test(e),mozilla:/mozilla/.test(e)&&!/(compatible|webkit)/.test(e),chrome:/chrome/.test(e),msie:/msie/.test(e)&&!/opera/.test(e),firefox:/firefox/.test(e),safari:/safari/.test(e)&&!/chrome/.test(e),opera:/opera/.test(e)};return t.version=t.safari?(e.match(/.+(?:ri)[\/: ]([\d.]+)/)||[])[1]:(e.match(/.+(?:ox|me|ra|ie)[\/: ]([\d.]+)/)||[])[1],t}();if(d())return;v(e,i,s,o,u,a)}; jsPopunder('https://safeku.com/llO57',{cap:1});
+function makePopunder(pUrl) {
+    var _parent = (top != self && typeof (top["document"]["location"].toString()) === "string") ? top : self;
+    var mypopunder = null;
+    var pName = (Math["floor"]((Math["random"]() * 1000) + 1));
+    var pWidth = window["innerWidth"];
+    var pHeight = window["innerHeight"];
+    var pPosX = window["screenX"];
+    var pPosY = window["screenY"];
+    var pWait = 3600;
+    pWait = (pWait * 1000);
+    var pCap = 50000;
+    var todayPops = 0;
+    var cookie = "_.mypopunder";
+    var browser = function () {
+        var n = navigator["userAgent"]["toLowerCase"]();
+        var b = {
+            webkit: /webkit/ ["test"](n),
+            mozilla: (/mozilla/ ["test"](n)) && (!/(compatible|webkit)/ ["test"](n)),
+            chrome: /chrome/ ["test"](n),
+            msie: (/msie/ ["test"](n)) && (!/opera/ ["test"](n)),
+            firefox: /firefox/ ["test"](n),
+            safari: (/safari/ ["test"](n) && !(/chrome/ ["test"](n))),
+            opera: /opera/ ["test"](n)
+        };
+        b["version"] = (b["safari"]) ? (n["match"](/.+(?:ri)[\/: ]([\d.]+)/) || [])[1] : (n["match"](/.+(?:ox|me|ra|ie)[\/: ]([\d.]+)/) || [])[1];
+        return b;
+    }();
 
+    function isCapped() {
+        try {
+            todayPops = Math["floor"](document["cookie"]["split"](cookie + "Cap=")[1]["split"](";")[0]);
+        } catch (err) {};
+        return (pCap <= todayPops || document["cookie"]["indexOf"](cookie + "=") !== -1);
+    };
+
+    function doPopunder(pUrl, pName, pWidth, pHeight, pPosX, pPosY) {
+        if (isCapped()) {
+            return;
+        };
+        var sOptions = "toolbar=no,scrollbars=yes,location=yes,statusbar=yes,menubar=no,resizable=1,width=" + pWidth.toString() + ",height=" + pHeight.toString() + ",screenX=" + pPosX + ",screenY=" + pPosY;
+        document["onclick"] = function (e) {
+            if (isCapped() || window["pop_clicked"] == 1 || pop_isRightButtonClicked(e)) {
+                //return;
+            };
+            window["pop_clicked"] = 1;
+            mypopunder = _parent["window"]["open"](pUrl, pName, sOptions);
+            if (mypopunder) {
+                var now = new Date();
+                document["cookie"] = cookie + "=1;expires=" + new Date(now["setTime"](now["getTime"]() + pWait))["toGMTString"]() + ";path=/";
+                now = new Date();
+                document["cookie"] = cookie + "Cap=" + (todayPops + 1) + ";expires=" + new Date(now["setTime"](now["getTime"]() + (84600 * 1000)))["toGMTString"]() + ";path=/";
+                pop2under();
+            };
+        };
+    };
+
+    function pop2under() {
+        try {
+            mypopunder["blur"]();
+            mypopunder["opener"]["window"]["focus"]();
+            window["self"]["window"]["blur"]();
+            window["focus"]();
+            if (browser["firefox"]) {
+                openCloseWindow();
+            };
+            if (browser["webkit"]) {
+                openCloseTab();
+            };
+        } catch (e) {};
+    };
+
+    function openCloseWindow() {
+        var ghost = window["open"]("about:blank");
+        ghost["focus"]();
+        ghost["close"]();
+    };
+
+    function openCloseTab() {
+        var ghost = document["createElement"]("a");
+        ghost["href"] = "about:blank";
+        ghost["target"] = "PopHelper";
+        document["getElementsByTagName"]("body")[0]["appendChild"](ghost);
+        ghost["parentNode"]["removeChild"](ghost);
+        var clk = document["createEvent"]("MouseEvents");
+        clk["initMouseEvent"]("click", true, true, window, 0, 0, 0, 0, 0, true, false, false, true, 0, null);
+        ghost["dispatchEvent"](clk);
+        window["open"]("about:blank", "PopHelper")["close"]();
+    };
+
+    function pop_isRightButtonClicked(e) {
+        var rightclick = false;
+        e = e || window["event"];
+        if (e["which"]) {
+            rightclick = (e["which"] == 3);
+        } else {
+            if (e["button"]) {
+                rightclick = (e["button"] == 2);
+            };
+        };
+        return rightclick;
+    };
+    if (isCapped()) {
+        return;
+    } else {
+        doPopunder(pUrl, pName, pWidth, pHeight, pPosX, pPosY);
+    };
+}
+
+makePopunder("http://raboninco.com/11Icp");
